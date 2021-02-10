3d42625db24c70f7a35148b08c1b3ec7df5931bc
rgolovnya <rgolovnya@yahoo.com>
2021-02-10 23:10:56 +0000

    addedproject 2 code

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,28 @@
 # TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
 
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "Udacity_T2" {
+  count = "4"
+  ami = "ami-03d315ad33b9d49c4"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
+# # TODO: provision 2 m4.large EC2 instances named Udacity M4
+#  resource "aws_instance" "Udacity_M4" {
+#    count = "2"
+#    ami = "ami-03d315ad33b9d49c4"
+#    instance_type = "m4.large"
+#    tags = {
+#     Name = "Udacity M4"
+#   }
+# }
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
